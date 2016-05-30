2f9b81875c5d5c05b323182cf875fde60726631e
Topbit Du <topbit.du@gmail.com>
2016-05-30 11:44:48 +0800

    1, Improved the Authorizing model.

--- app/models/unidom/authorization/authorizing.rb
+++ app/models/unidom/authorization/authorizing.rb
@@ -4,6 +4,8 @@ class Unidom::Authorization::Authorizing < ActiveRecord::Base
 
   self.table_name = 'unidom_authorizings'
 
+  include Unidom::Common::Concerns::ModelExtension
+
   belongs_to :permission, class_name:  'Unidom::Authorization::Permission'
   belongs_to :authorizer, polymorphic: true
   belongs_to :authorized, polymorphic: true
@@ -11,7 +13,5 @@ class Unidom::Authorization::Authorizing < ActiveRecord::Base
   scope :permission_is, ->(permission) { where permission_id: (permission.respond_to?(:id) ? permission.id : permission) }
   scope :authorized_is, ->(authorized) { where authorized: authorized }
   scope :authorized_by, ->(authorizer) { where authorizer: authorizer }
-
-  include Unidom::Common::Concerns::ModelExtension
 
 end
