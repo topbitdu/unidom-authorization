9aab9e0d9cff90286f92106d348bb30f1844028e
Topbit Du <topbit.du@gmail.com>
2016-06-06 21:46:45 +0800

    1, Improved the Permission model for coding style.

--- app/models/unidom/authorization/permission.rb
+++ app/models/unidom/authorization/permission.rb
@@ -4,13 +4,13 @@ class Unidom::Authorization::Permission < ActiveRecord::Base
 
   self.table_name = 'unidom_permissions'
 
+  include Unidom::Common::Concerns::ModelExtension
+
   has_many :authorizings, class_name: 'Unidom::Authorization::Authorizing'
 
   validates :name, presence:    true, length: { in: 2..self.columns_hash['name'].limit }
   validates :path, allow_blank: true, length: { in: 2..self.columns_hash['path'].limit }
 
   scope :path_is, ->(path) { where path: path }
-
-  include Unidom::Common::Concerns::ModelExtension
 
 end
