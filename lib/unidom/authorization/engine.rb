a67be11f55b4cdd7149109f4d9d1deb0ba50e192
Topbit Du <topbit.du@gmail.com>
2017-01-01 12:05:27 +0800

    1, Improve the Engine class to include the Engine Extension concern.

--- lib/unidom/authorization/engine.rb
+++ lib/unidom/authorization/engine.rb
@@ -3,11 +3,11 @@ module Unidom
 
     class Engine < ::Rails::Engine
 
+      include Unidom::Common::EngineExtension
+
       isolate_namespace ::Unidom::Authorization
 
-      initializer :append_migrations do |app|
-        config.paths['db/migrate'].expanded.each { |expanded_path| app.config.paths['db/migrate'] << expanded_path } unless app.root.to_s.match root.to_s
-      end
+      enable_initializer enum_enabled: false, migration_enabled: true
 
     end
 
