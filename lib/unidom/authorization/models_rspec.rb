a78d468c0a57e96adfe21a4bee9e85aab09ed7c3
Topbit Du <topbit.du@gmail.com>
2017-01-28 10:26:41 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/authorization/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/authorization/authorizing_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Authorization::Authorizing'
+require 'rspec/models/unidom/authorization/permission_spec'  unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Authorization::Permission'
