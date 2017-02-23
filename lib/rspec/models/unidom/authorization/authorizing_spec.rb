3b7d34260859e6214078fa3d3354d78a5b7c64ca
Topbit Du <topbit.du@gmail.com>
2017-02-23 11:41:29 +0800

    1, Improve the Authorizing spec for the permission_is scope.

--- lib/rspec/models/unidom/authorization/authorizing_spec.rb
+++ lib/rspec/models/unidom/authorization/authorizing_spec.rb
@@ -18,6 +18,14 @@ describe Unidom::Authorization::Authorizing, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    #it_behaves_like 'belongs_to', model_attributes, :permission, Unidom::Authorization::Permission, { name: 'User Management', path: 'administration/users'}
+
+    it_behaves_like 'scope', :permission_is, [
+      { attributes_collection: [ model_attributes ], count_diff: 1, args: [ model_attributes[:permission_id] ] },
+      { attributes_collection: [ model_attributes ], count_diff: 1, args: [ Unidom::Authorization::Permission.new(id: model_attributes[:permission_id]) ] },
+      { attributes_collection: [ model_attributes ], count_diff: 0, args: [ model_attributes[:authorized_id] ] },
+      { attributes_collection: [ model_attributes ], count_diff: 0, args: [ Unidom::Authorization::Permission.new(id: model_attributes[:authorized_id]) ] } ]
+
   end
 
 end
