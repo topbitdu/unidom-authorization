e49406ef78adcedcd17d5f66ee772fb738f8a586
Topbit Du <topbit.du@gmail.com>
2017-03-20 15:57:50 +0800

    1, As Permission shared examples.

--- lib/rspec/models/unidom/authorization/permission_spec.rb
+++ lib/rspec/models/unidom/authorization/permission_spec.rb
@@ -13,18 +13,19 @@ describe Unidom::Authorization::Permission, type: :model do
       path: 'users'
     }
 
-    #name_max_length = described_class.columns_hash['name'].limit
-    #path_max_length = described_class.columns_hash['path'].limit
-
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
-    it_behaves_like 'validates text', model_attributes, :name, length: 2..described_class.columns_hash['name'].limit
-    it_behaves_like 'validates text', model_attributes, :path, length: 2..described_class.columns_hash['path'].limit
+    it_behaves_like 'validates text', model_attributes, :name,
+      length: 2..described_class.columns_hash['name'].limit
+    it_behaves_like 'validates text', model_attributes, :path,
+      length: 2..described_class.columns_hash['path'].limit
 
     it_behaves_like 'scope', :path_is, [
       { attributes_collection: [ model_attributes ], count_diff: 1, args: [ model_attributes[:path]           ] },
       { attributes_collection: [ model_attributes ], count_diff: 0, args: [ "/#{model_attributes[:path]}"     ] },
       { attributes_collection: [ model_attributes ], count_diff: 0, args: [ "#{model_attributes[:path]}.html" ] } ]
+
+    it_behaves_like 'Unidom::Authorization::Concerns::AsPermission', model_attributes
 
   end
 
