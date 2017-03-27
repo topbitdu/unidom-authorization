63c37caa7b89991134bed881696c90da384e8b84
Topbit Du <topbit.du@gmail.com>
2017-03-27 21:11:36 +0800

    1, Improve the As Authorized shared examples for the #is_authorized! method, the #is_authorized? method, & the #is_prohibited! method.

--- lib/rspec/models/unidom/authorization/concerns/as_authorized_shared_examples.rb
+++ lib/rspec/models/unidom/authorization/concerns/as_authorized_shared_examples.rb
@@ -1,17 +1,26 @@
-shared_examples 'Unidom::Authorization::Concerns::AsAuthorized' do |model_attributes|
+shared_examples 'Unidom::Authorization::Concerns::AsAuthorized' do |model_attributes, permission, authorizer|
 
-  authorizing_1_attribtues = {
-      permission_id:   SecureRandom.uuid,
-      authorizer_id:   SecureRandom.uuid,
-      authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
-    }
+  context do
 
-  authorizing_2_attribtues = {
-      permission_id:   SecureRandom.uuid,
-      authorizer_id:   SecureRandom.uuid,
-      authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
-    }
+    authorizing_1_attribtues = {
+        permission_id:   SecureRandom.uuid,
+        authorizer_id:   SecureRandom.uuid,
+        authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
+      }
 
-  it_behaves_like 'has_many', model_attributes, :authorizings, Unidom::Authorization::Authorizing, [ authorizing_1_attribtues, authorizing_2_attribtues ]
+    authorizing_2_attribtues = {
+        permission_id:   SecureRandom.uuid,
+        authorizer_id:   SecureRandom.uuid,
+        authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
+      }
+
+    it_behaves_like 'has_many', model_attributes, :authorizings, Unidom::Authorization::Authorizing, [ authorizing_1_attribtues, authorizing_2_attribtues ]
+
+    authorized = described_class.create! model_attributes
+    it_behaves_like 'assert_present!', authorized, :is_authorized!, [ { permission: permission, by: authorizer, at: Time.now } ], [ :permission, :by, :at ]
+    it_behaves_like 'assert_present!', authorized, :is_authorized?, [ { permission: permission,                 at: Time.now } ], [ :permission,      :at ]
+    it_behaves_like 'assert_present!', authorized, :is_prohibited!, [ { permission: permission,                 at: Time.now } ], [ :permission,      :at ]
+
+  end
 
 end
