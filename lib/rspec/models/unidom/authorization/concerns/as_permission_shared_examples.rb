ea8716d105b58a5e9633a8f0dd3d24ef41d7a729
Topbit Du <topbit.du@gmail.com>
2017-03-29 21:12:42 +0800

    1, Improve the As Permission shared examples for the #authorize! method, the #authorize? method, & the #prohibit! method.

--- lib/rspec/models/unidom/authorization/concerns/as_permission_shared_examples.rb
+++ lib/rspec/models/unidom/authorization/concerns/as_permission_shared_examples.rb
@@ -1,19 +1,28 @@
-shared_examples 'Unidom::Authorization::Concerns::AsPermission' do |model_attributes|
+shared_examples 'Unidom::Authorization::Concerns::AsPermission' do |model_attributes, authorized, authorizer|
 
-  authorizing_1_attribtues = {
-      authorized_id:   SecureRandom.uuid,
-      authorized_type: 'Unidom::Authorization::Authorized::Mock',
-      authorizer_id:   SecureRandom.uuid,
-      authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
-    }
+  context do
 
-  authorizing_2_attribtues = {
-      authorized_id:   SecureRandom.uuid,
-      authorized_type: 'Unidom::Authorization::Authorized::Mock',
-      authorizer_id:   SecureRandom.uuid,
-      authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
-    }
+    authorizing_1_attribtues = {
+        authorized_id:   SecureRandom.uuid,
+        authorized_type: 'Unidom::Authorization::Authorized::Mock',
+        authorizer_id:   SecureRandom.uuid,
+        authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
+      }
 
-  it_behaves_like 'has_many', model_attributes, :authorizings, Unidom::Authorization::Authorizing, [ authorizing_1_attribtues, authorizing_2_attribtues ]
+    authorizing_2_attribtues = {
+        authorized_id:   SecureRandom.uuid,
+        authorized_type: 'Unidom::Authorization::Authorized::Mock',
+        authorizer_id:   SecureRandom.uuid,
+        authorizer_type: 'Unidom::Authorization::Authorizer::Mock'
+      }
+
+    it_behaves_like 'has_many', model_attributes, :authorizings, Unidom::Authorization::Authorizing, [ authorizing_1_attribtues, authorizing_2_attribtues ]
+
+    permission = described_class.create! model_attributes
+    it_behaves_like 'assert_present!', permission, :authorize!, [ authorized, { by: authorizer, at: Time.now } ], [ { 0 => :authorized }, :by, :at ]
+    it_behaves_like 'assert_present!', permission, :authorize?, [ authorized, {                 at: Time.now } ], [ { 0 => :authorized },      :at ]
+    it_behaves_like 'assert_present!', permission, :prohibit!,  [ authorized, {                 at: Time.now } ], [ { 0 => :authorized },      :at ]
+
+  end
 
 end
