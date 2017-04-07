a0f213895754f9fa6a183be3491489c2e810360e
Topbit Du <topbit.du@gmail.com>
2017-04-07 09:18:03 +0800

    1, Improve the As Authorized shared examples for the #is_authorized! method, the #is_authorized? method.

--- lib/rspec/models/unidom/authorization/concerns/as_authorized_shared_examples.rb
+++ lib/rspec/models/unidom/authorization/concerns/as_authorized_shared_examples.rb
@@ -1,5 +1,46 @@
 shared_examples 'Unidom::Authorization::Concerns::AsAuthorized' do |model_attributes, permission, authorizer|
 
+  before :each do
+    @authorized = described_class.create! model_attributes
+  end
+
+  after :each do
+  end
+
+  context '#is_authorized!' do
+
+    it "should be able to be authorized by #{authorizer.inspect}" do
+      authorizing = @authorized.is_authorized! permission: permission, by: authorizer
+      expect(authorizing).to     be_present
+      expect(authorizing).to     be_a(Unidom::Authorization::Authorizing)
+      expect(authorizing).not_to be_new_record
+    end
+
+    it "should be able to be authorized by #{authorizer.inspect}, at #{Time.now.inspect}" do
+      authorizing = @authorized.is_authorized! permission: permission, by: authorizer, at: Time.now
+      expect(authorizing).to     be_present
+      expect(authorizing).to     be_a(Unidom::Authorization::Authorizing)
+      expect(authorizing).not_to be_new_record
+    end
+
+  end
+
+  context '#is_authorized?' do
+
+    it "should be able to be authorized by #{authorizer.inspect}" do
+      expect(@authorized.is_authorized? permission: permission).to be_falsey
+      @authorized.is_authorized!        permission: permission, by: authorizer
+      expect(@authorized.is_authorized? permission: permission).to be_truthy
+    end
+
+    it "should be able to be authorized by #{authorizer.inspect}, at #{Time.now.inspect}" do
+      expect(@authorized.is_authorized? permission: permission, at: Time.now).to be_falsey
+      @authorized.is_authorized!        permission: permission, by: authorizer, at: Time.now
+      expect(@authorized.is_authorized? permission: permission, at: Time.now).to be_truthy
+    end
+
+  end
+
   context do
 
     authorizing_1_attribtues = {
