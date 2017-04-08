5f2d3f7c227598cd372f56b96c0d1761e1f31006
Topbit Du <topbit.du@gmail.com>
2017-04-08 09:24:06 +0800

    1, Improve the As Permission shared examples for the #authorize! method, the #authorize? method.

--- lib/rspec/models/unidom/authorization/concerns/as_permission_shared_examples.rb
+++ lib/rspec/models/unidom/authorization/concerns/as_permission_shared_examples.rb
@@ -1,5 +1,46 @@
 shared_examples 'Unidom::Authorization::Concerns::AsPermission' do |model_attributes, authorized, authorizer|
 
+  before :each do
+    @permission = described_class.create! model_attributes
+  end
+
+  after :each do
+  end
+
+  context '#authorize!' do
+
+    it "should be able to authorize #{authorized.inspect} by #{authorizer.inspect}" do
+      authorizing = @permission.authorize! authorized, by: authorizer
+      expect(authorizing).to     be_present
+      expect(authorizing).to     be_a(Unidom::Authorization::Authorizing)
+      expect(authorizing).not_to be_new_record
+    end
+
+    it "should be able to authorize #{authorized.inspect} by #{authorizer.inspect}, at #{Time.now.inspect}" do
+      authorizing = @permission.authorize! authorized, by: authorizer, at: Time.now
+      expect(authorizing).to     be_present
+      expect(authorizing).to     be_a(Unidom::Authorization::Authorizing)
+      expect(authorizing).not_to be_new_record
+    end
+
+  end
+
+  context '#authorize?' do
+
+    it "should be able to authorize #{authorized.inspect} by #{authorizer.inspect}" do
+      expect(@permission.authorize? authorized).to be_falsey
+      @permission.authorize!        authorized, by: authorizer
+      expect(@permission.authorize? authorized).to be_truthy
+    end
+
+    it "should be able to authorize #{authorized.inspect} by #{authorizer.inspect}, at #{Time.now.inspect}" do
+      expect(@permission.authorize? authorized, at: Time.now).to be_falsey
+      @permission.authorize!        authorized, by: authorizer, at: Time.now
+      expect(@permission.authorize? authorized, at: Time.now).to be_truthy
+    end
+
+  end
+
   context do
 
     authorizing_1_attribtues = {
