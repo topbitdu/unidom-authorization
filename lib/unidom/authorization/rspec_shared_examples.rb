281dd8af214489904f7eaa11ba5c5858f6db2bf7
Topbit Du <topbit.du@gmail.com>
2017-04-24 09:14:26 +0800

    1, Improve the RSpec shared examples manifest to require the Authorizing shared examples.

--- lib/unidom/authorization/rspec_shared_examples.rb
+++ lib/unidom/authorization/rspec_shared_examples.rb
@@ -1,3 +1,4 @@
 require 'rspec/models/unidom/authorization/concerns/as_authorized_shared_examples'
 require 'rspec/models/unidom/authorization/concerns/as_permission_shared_examples'
+require 'rspec/models/unidom/authorization/authorizing_shared_examples'
 require 'rspec/models/unidom/authorization/permission_shared_examples'
