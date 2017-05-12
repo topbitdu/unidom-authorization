3aeae20c16b7663da437890f56d5e8bd7e96264d
Topbit Du <topbit.du@gmail.com>
2017-05-12 10:02:28 +0800

    1, Improve the Permission migration for the #opened_at column, & the #closed_at column.

--- db/migrate/20000401000000_create_unidom_permissions.rb
+++ db/migrate/20000401000000_create_unidom_permissions.rb
@@ -10,8 +10,8 @@ class CreateUnidomPermissions < ActiveRecord::Migration
       t.text :description
 
       t.column   :state, 'char(1)', null: false, default: 'C'
-      t.datetime :opened_at,        null: false, default: ::Time.utc(1970)
-      t.datetime :closed_at,        null: false, default: ::Time.utc(3000)
+      t.datetime :opened_at,        null: false, default: Unidom::Common::OPENED_AT
+      t.datetime :closed_at,        null: false, default: Unidom::Common::CLOSED_AT
       t.boolean  :defunct,          null: false, default: false
       t.jsonb    :notation,         null: false, default: {}
 
